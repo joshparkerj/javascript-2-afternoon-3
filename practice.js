fbf0522f4fefdab4a123c49057173a8201dd7372
joshparkerj <superfakek@gmail.com>
2018-08-25 22:01:41 -0600

    complete code challenges

--- practice.js
+++ practice.js
@@ -28,7 +28,9 @@
 */
 
 // Code Here 
-
+function first (arr,cb){
+  cb(arr[0]);
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,6 +50,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr,cb){
+  cb(arr[arr.length-1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,7 +71,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1,num2,cb){
+  cb(num1*num2);
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +92,9 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains(arr,name,cb){
+  cb(arr.includes(name));
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,7 +115,9 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(arr,cb){
+  cb(arr.filter((e,i)=>arr.indexOf(e)===i));
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +134,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each(names,cb){
+  for (i in names){
+    cb(names[i],Number(i));
+  }
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,7 +155,14 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById(users,id,cb){
+  for (user of users){
+    if (user.id === id){
+      cb(user);
+      break;
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
